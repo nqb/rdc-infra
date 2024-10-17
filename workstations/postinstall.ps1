8bdb04724771fe36522a3351d64ce65c92f6bf8f
nqb <nqb+git@azyx.fr>
2024-10-17 14:22:02 +0200

    improve post install (2)

--- workstations/postinstall.ps1
+++ workstations/postinstall.ps1
@@ -1,13 +1,12 @@
-$glpi_agent_ver = "1.1.0"
+$glpi_agent_ver = "1.11.0"
 $firefoxesr_ver = "128.3.1"
 $thunderbird_ver = "115.12.2"
 $element_ver = "1.11.80"
 
+$ErrorActionPreference = "stop"
+
 Write-Output "Install latest version of Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-Write-Output "Install GLPI agent $glpi_agent_ver and send FIRST inventory"
-choco install glpi-agent -y --version $glpi_agent_ver --install-arguments="SERVER=https://glpi.rennesducompost.fr/ TAG=InstalledByChocolatey RUNNOW=1"
 
 Write-Output "Install Firefox ESR $firefoxesr_ver"
 choco install firefoxesr -y --version $firefoxesr_ver
@@ -20,3 +19,6 @@ choco install element -y --version $element_ver
 
 Write-Output "Install latest version of common softwares"
 choco install vlc 7zip okular pdfsam libreoffice-still -y
+
+Write-Output "Install GLPI agent $glpi_agent_ver and send FIRST inventory"
+choco install glpi-agent -y --version $glpi_agent_ver --install-arguments="SERVER=https://glpi.rennesducompost.fr/ TAG=InstalledByChocolatey RUNNOW=1"
