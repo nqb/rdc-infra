6f763259b9f9d6cc58518a2ab64179b7e6af1f43
nqb <nqb+git@azyx.fr>
2024-10-17 15:05:37 +0200

    use powershell commands to update WinRM listener
    id

--- workstations/setup_winrm.ps1
+++ workstations/setup_winrm.ps1
@@ -1,4 +1,4 @@
-# Source: https://developer.hashicorp.com/packer/docs/communicators/winrm
+# Configure WinRM for PSRemoting through HTTPS
 Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
 
 # Don't set this before Set-ExecutionPolicy as it throws an error
@@ -6,7 +6,7 @@ $ErrorActionPreference = "stop"
 
 # start + do a lot of stuff
 # see https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/enable-psremoting
-Enable-PSRemoting –SkipNetworkProfileCheck -Force
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
 
 # Remove HTTP listener
 Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
@@ -17,21 +17,23 @@ $hostname = hostname
 $Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "$hostname"
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
 
-write-output "Configure WinRM to allow unencrypted communication through HTTPS"
+Write-output "Configure WinRM to allow unencrypted communication through HTTPS"
 
 # Hack to avoid next command to fail because network profile is public
 Set-NetConnectionProfile -NetworkCategory Private
 
-cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"$hostname`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
+# Change WinRM config
+Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $true
+Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
+Set-Item -Path WSMan:\localhost\Service\Auth\CredSSP -Value $true
+
+Set-Item -Path WSMan:\localhost\Client\Basic -Value $true
+Set-Item -Path WSMan:\localhost\Client\AllowUnencrypted -Value $true
+
+Set-WSManInstance -ResourceURI winrm/config/listener -SelectorSet @{address="*";transport="https"} -ValueSet @{Hostname="$hostname"}
 
 # Switch back to public
 Set-NetConnectionProfile -NetworkCategory Public
 
 # Add firewall rule to allow WinRM on HTTPS
 New-NetFirewallRule -Displayname 'WINRM-HTTPS-In-TCP-Any' -Name 'WINRM-HTTPS-In-TCP-Any' -Profile Any -LocalPort 5986 -Protocol TCP
-
